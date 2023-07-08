e55b331d19a02ce3ff05d850d3813578f541f38e
Shoaib Ali Asif <133796092+Shoaib-Ali-ASif@users.noreply.github.com>
2023-07-08 16:01:00 +0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-    // $user = $_SESSION['user'];
-    header('location: ./login.php'); 
-}
 // session_destroy();
 unset($_SESSION['user']);
 header('location: ./login.php');
